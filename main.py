9500e677850d2c8e7a994cfb961a5fc14ad39698
Tagir <khalilov.tg@gmail.com>
2020-11-03 17:34:39 +0300

    Add variables

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import json
 #https://vk.com/friends
 
 import  post
@@ -20,7 +21,9 @@ os.system("cls||clear")
 
 #status = requests.get("https://vk.com")
 
-comms = {"help": "connect [-url value]\ndisconnect []"}
+comms = {"help": "connect [-url value]\ndisconnect []\nprint [value] [$variable]\nvar [name] [value]"}
+env_var = {}
+
 сur_comm = []
 
 _input = ""
@@ -32,7 +35,31 @@ def main_line():
     _input = input()
     cur_comm = _input.split()
 
-    print(cur_comm)
-    print(comms.get(cur_comm[0]))
+    comm_handler()
+
+def comm_handler():
+    global cur_comm
+    global env_var
+
+
+    all = ""
+    if (cur_comm[0] == "print"):
+        for i in range(1, len(cur_comm)):
+            all += cur_comm[i]
+
+        words = all.split()
+
+        for word in words:
+            if ('$' in word):
+                print(env_var.get(word[1:]), end="")
+            else:
+                print(word, end="")
+
+        print()
+        main_line()
+
+    elif (cur_comm[0] == "var"):
+        env_var.update({cur_comm[1]:cur_comm[2]})
+        main_line()
 
 main_line()
